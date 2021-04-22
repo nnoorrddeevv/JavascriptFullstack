9b77e3e7dee8b25c056c59ec41d50107d474578a
nnoorrddeevv <philip@nordev.tech>
2021-04-22 14:58:20 +0200

    dawdaw

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="contact">
     <h1>This is the contact page</h1>
-    <form 
+    <!-- <form 
     name="contact" 
     method="POST" 
-    netlify
+    data-netlify="true"
     data-netlify-honeypot="bot-field"
     >
       <input type="hidden" name="form-name" value="ask-question" />
       <label>Your Name: <input type="text" name="name" /></label>   
       <label>Your Email: <input type="email" name="email" /></label>
       <label>Message: <textarea name="message"></textarea></label>
-      <router-link to="/contact">
+      <div netlify-recaptcha></div>
+
+    </form> -->
+    <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+          <router-link to="/contact">
         <button type="submit">Send</button>
       </router-link>
-    </form>
+  </p>
+</form>
   </div>
 </template>
 
