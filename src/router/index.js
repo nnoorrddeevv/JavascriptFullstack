8e603b97b2117b66e1ae8154c2615eae585c835a
nnoorrddeevv <philip@nordev.tech>
2021-04-22 13:48:27 +0200

    io

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,10 @@ const routes =
     name: 'Contact',
     component: () => import( '../views/Contact.vue')
   },{
+    path: '/contact/post',
+    name: 'Contact',
+    component: () => import( '../views/Contact.vue')
+  },{
     path: '/products',
     name: 'Products',
     component: () => import( '../views/Products.vue')
